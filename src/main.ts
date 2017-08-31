2fef45e9cdf7ea80c225f0f2858a62f1d407ae10
Nikolai Burak <nikolyasha2205@gmail.com>
2017-08-31 16:38:28 +0300

    fix bugs with provider

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import {InformationComponent} from './information/information.component';
-import {CreateProductComponent} from './create-product/create-product.component';
-
 /// <reference types='node' />
 
 if (environment.production) {
